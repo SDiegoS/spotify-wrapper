63b469fbd0ad25c492d340c71e83094c8a861993
Diego Ireno dos Santos <diego_ireno@hotmail.com>
2020-06-11 14:29:00 -0300

    atualizando arquivos

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-export const search = () => {
-  fetch(('https://api.spotify.com/v1/search'));
+export const search = (query, type) => {
+  fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`)
+    .then(data => data.json());
+    // .then(data => data.json());
 
 };
-export const searchAlbums = () => {};
 export const searchArtists = () => {};
+export const searchAlbums = () => {};
 export const searchTracks = () => {};
 export const searchPlaylists = () => {};
 
